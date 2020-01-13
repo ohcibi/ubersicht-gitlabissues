fcf23dc3e49613a6e2ed39429060ae47289c5c6c
Tobias Witt <tobias.witt@hhu.de>
2020-01-13 11:31:29 +0100

    Fix gitlab access

--- gitlabissues.widget/index.jsx
+++ gitlabissues.widget/index.jsx
@@ -12,6 +12,8 @@
  *
  * END HEADER
  */
+import { css } from "uebersicht"
+
 
 export const params = {
     'gitlab_url': 'gitlab.com', //url of gitlab instance
@@ -21,100 +23,102 @@ export const params = {
     'update_frequency': 5 // number of minutes between updates
 }
 
-export const command = (dispatch) =>
-  fetch('https://' + params.gitlab_url + '/api/v4/projects/' + params.project_id + '/issues?private_token=' + params.access_token + '&state=opened')
-    .then((resp) => {
-      resp.json().then(data => {
-        dispatch({ type: 'FETCH_SUCCEDED', data: data });
-      })
-    })
-    .catch((error) => {
-      dispatch({ type: 'FETCH_FAILED', error: error });
-    });
+export const command = (dispatch) => {
+  return fetch('https://' + params.gitlab_url + '/api/v4/projects/' + params.project_id + '/issues?private_token=' + params.access_token + '&state=opened')
+    .then(resp => resp.json())
+    .then(data => { dispatch({ type: 'FETCH_SUCCEDED', data }); })
+    .catch(error => { dispatch({ type: 'FETCH_FAILED', error }); });
+};
 
 export const refreshFrequency = parseInt(params.update_frequency * 60000); 
 
-export const render = (data) => {
-  return data.error ? (
-    <div>Unable to retrieve issues: <strong>{String(error)}</strong></div>
-  ) : (
-    <div>
-      <h1>Gitlab Issues</h1>
-      <h4>Updated {data.updated}</h4>
-      <table className={table}>
-        <thead>
-          <tr>
-            <td className={tableHeader}>Number</td>
-            <td className={tableHeader}>Issue</td>
-            <td className={tableHeader}>Author</td>
-            <td className={tableHeader}>Created</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data.issues.map((issue, i) => {
-            if(i%2 == 0) {
-              var rowClass = css({
-                padding: 5
-              })
-            }
-            else {
-              var rowClass = css({
-                backgroundColor: 'rgba(160, 160, 160, 0.1)',
-                padding: 5
-              })
-            }
-            return (
-              <tr key={i}>
-                <td className={rowClass}>
-                  #{issue.iid}
-                </td>
-                <td className={rowClass}>
-                  {issue.title}
-                </td>
-                <td className={rowClass}>
-                  <img src={issue.author_avatar} height='16' width='16'/>  {issue.author}
-                </td> 
-                <td className={rowClass}>
-                  {issue.created}
-                </td>
-              </tr>
-            )})}
-        </tbody>
-      </table>
-    </div>
-  );
+export const render = ({output, updated, error}) => {
+  if (error) {
+    return (<div>Unable to retrieve issues: <strong>{String(error)}</strong></div>);
+  }
+
+  if (output.constructor === Array) {
+    return (
+      <div>
+        <h1>Gitlab Issues <small>(Updated at: {updated})</small></h1>
+        <table className={table}>
+          <thead>
+            <tr>
+              <td className={tableHeader}>Number</td>
+              <td className={tableHeader}>Issue</td>
+              <td className={tableHeader}>Author</td>
+              <td className={tableHeader}>Created</td>
+            </tr>
+          </thead>
+          <tbody>
+            {output.map((issue, i) => {
+              if (i % 2 == 0) {
+                var rowClass = css({
+                  padding: 5
+                })
+              }
+              else {
+                var rowClass = css({
+                  backgroundColor: 'rgba(160, 160, 160, 0.1)',
+                  padding: 5
+                })
+              }
+              return (
+                <tr key={i}>
+                  <td className={rowClass}>
+                    #{issue.iid}
+                  </td>
+                  <td className={rowClass}>
+                    {issue.title}
+                  </td>
+                  <td className={rowClass}>
+                    <img src={issue.author_avatar} height='16' width='16' />  {issue.author}
+                  </td>
+                  <td className={rowClass}>
+                    {issue.created}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return output;
 }
 
 export const initialState = { output: 'Fetching issues...' };
 
 export const updateState = (event, previousState) => {
   if (event.error) {
-    return { ...previousState, warning: `Error: ${event.error}` };
+    console.warn(event.error);
+    return { ...previousState, warning: `Error occured: ${event.error}` }
   }
 
-  previousState.issues = [];
+  if (event.type !== 'FETCH_SUCCEDED') {
+    return previousState;
+  }
 
-  var i = 0;
-  for(let issue of event.data) {
-    if(i >= params.issue_count) break;
-    i += 1;
-
-    previousState.issues.push({
-      'iid': issue.iid,
-      'title': issue.title,
-      'author': issue.author.name,
-      'author_avatar': issue.author.avatar_url,
-      'created': issue.created_at,
-      'url': issue.web_url
+  const now = new Date();
+  return {
+    updated: `${now.getHours()}:${now.getMinutes()}`,
+    output: event.data.slice(0, params.issue_count).map(issue => {
+      const created = new Date(issue.created_at);
+      const month = (month => month)(created)
+      debugger
+      return {
+        iid: issue.iid,
+        title: issue.title,
+        author: issue.author.name,
+        author_avatar: issue.author.avatar_url,
+        created: `${created.getFullYear()}-${created.getMonth() + 1}-${created.getDate()} ${created.getHours()}:${created.getMinutes()}`,
+        url: issue.web_url
+      };
     })
-  }
-
-  previousState.updated = new Date().toString();
-  return previousState;
+  };
 }
-
-
-import { css } from "uebersicht"
 
 export const className = {
   top: 10,
